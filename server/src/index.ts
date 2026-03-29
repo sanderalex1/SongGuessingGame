a90c9c1502f80361a1305441c8835df1bf05b405
sanderalex1 <oleksandr.kodehode@gmail.com>
2026-03-29 15:07:26 +0200

    add Socket.IO server with basic connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,32 @@
 import app from "./app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 const PORT: number = 3000;
 
-app.listen(PORT, () => {
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("ping:send", (data) => {
+    console.log("Received ping:", data);
+    socket.emit("pong:receive", {
+      message: "Hello from server!",
+      yourData: data,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
